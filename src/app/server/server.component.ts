9a9a2fd30028a98463e655d352bf6caf324a9201
Tomoko <haisaitomoco@gmail.com>
2021-03-04 10:36:52 +0200

    https://github.com/Tomoko-hub/angular-tutorial-myNewDreamApp.git

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-server',
+  //selector:'[app-server]',
+  //selector: '.app-server',
+  templateUrl: './server.component.html',
+  styleUrls: ['./server.component.css']
+})
+export class ServerComponent implements OnInit {
+
+  serverId: number = 19;
+  serverStatus: String = 'offline';
+
+  getServerStatus(){
+    return this.serverStatus;
+  }
+
+  getColor(){
+    return this.serverStatus === 'online' ? 'green' : 'red';
+  }
+
+  constructor() { 
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
+
+  ngOnInit(): void {
+  }
+
+}
